d770355714c47d768550c11c11ed0f1e1a05a8b1
Harshit Kumar Singh <hksingh.dev@gmail.com>
2022-11-12 16:58:56 +0530

    add tab view in CSS

--- index.js
+++ index.js
@@ -1,31 +1,17 @@
-const nav = document.querySelector('.primary-navigation') 
-const navToggle = document.querySelector('.mobile-menu-toggle')
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-menu-toggle");
 
+navToggle.addEventListener("click", () => {
+  const visiblity = nav.getAttribute("data-visible");
+  if (visiblity === "false") {
+    nav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+    document.querySelector("body").style.background = "burlywood";
+  } else {
+    nav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+    document.querySelector("body").style.background = "none";
+  }
 
-
-// when someone clicks the hamburger button
-
-navToggle.addEventListener('click',()=>{
-
-    // if the menu is closed, open it
-
-    const visiblity = nav.getAttribute("data-visible")
-    if (visiblity === "false"){
-        nav.setAttribute("data-visible",true)
-        navToggle.setAttribute("aria-expanded",true)
-        document.querySelector('body').style.background="burlywood"
-        
-    }else{
-        nav.setAttribute("data-visible",false)
-        navToggle.setAttribute("aria-expanded",false)
-        document.querySelector('body').style.background="none"
-    }
-
-    // console.log(visiblity)
-    console.log(navToggle.getAttribute('aria-expanded'))
-
-    // if the menu is open, close it
-
-
-})
-
+  console.log(navToggle.getAttribute("aria-expanded"));
+});
